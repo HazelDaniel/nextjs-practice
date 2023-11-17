8adea64fc1c5308e5bfa896efe815c52a03a39fc
HazelDaniel <hazeldaniel856@gmail.com>
2023-11-17 09:31:19 +0100

    excluded the nextjs-dashboard to it's separate repo

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++
@@ -1,4 +0,0 @@
-const Page = () => {
-  return <h2>Customers</h2>;
-};
-export default Page;
--- nextjs-dashboard/app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-import { ReactNode } from "react";
-
-const Page = ({ children }: { children: ReactNode }) => {
-  return <h2>Dashboard Page</h2>;
-};
-
-export default Page;
